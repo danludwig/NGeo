6da15fc3ac257e4ea901a7e33a38eb89f1787d89
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2013-02-28 09:31:26 -0500

    Remove Yahoo PlaceFinder (service no longer exists).

--- NGeo.Tests/ExtensionMethodTests.cs
+++ NGeo.Tests/ExtensionMethodTests.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NGeo.Yahoo.PlaceFinder;
 using Should;
 using System;
 
@@ -8,24 +7,6 @@ namespace NGeo
     [TestClass]
     public class ExtensionMethodTests
     {
-        [TestMethod]
-        public void ExtensionMethod_GetEnumMemberAttribute_ShouldReturnNull_WhenEnumHasNoMemberAttribute()
-        {
-            var attribute = QualityCategory.Area.GetEnumMemberAttribute();
-            attribute.ShouldBeNull();
-        }
-
-        [TestMethod]
-        public void ExtensionMethod_ApplyDefaultValues_ShouldApplyDefaultValueAttributes()
-        {
-            var request = new PlaceByFreeformText("test");
-            request.ApplyDefaultValues();
-            request.Locale.ShouldEqual("en-US");
-            request.Start.ShouldEqual(0);
-            request.Count.ShouldEqual(10);
-            request.Offset.ShouldEqual(15);
-        }
-
         [TestMethod]
         public void ExtensionMethod_ToNullIfEmptyOrWhiteSpace_ShouldReturnNull_WhenInputIsNull()
         {
--- NGeo/Yahoo/PlaceFinder/QualityCategory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NGeo.Yahoo.PlaceFinder
-{
-    public enum QualityCategory
-    {
-        Area = 0,
-        Line = 70,
-        Point = 80,
-    }
-}
